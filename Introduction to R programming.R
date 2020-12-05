06eac6184747ac44c9b5dcad7ef04e298c7424be
Anshika Dhingra <72697436+anshikadhingra@users.noreply.github.com>
2020-12-05 19:24:30 +0530

    Basic R coding

---
+++ Introduction to R programming.R
@@ -0,0 +1,10 @@
+#Introduction
+print("Welcome to Analytics Edge class")
+a<-("Welcome to Analytics Edge class")
+#a<-(print("Welcome to Analytics Edge class"))
+a
+
+#plotting
+X=1:5
+Y=6:10
+plot(X,Y) 
